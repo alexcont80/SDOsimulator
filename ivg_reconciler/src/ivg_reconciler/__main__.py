9811b31348b9a836a1e902c35c2b8be4fcd7df62
alexcont80 <alexcont80@gmail.com>
2026-09-18 11:16:33 +0200

    Fix PyInstaller package imports

--- ivg_reconciler/src/ivg_reconciler/__main__.py
+++ ivg_reconciler/src/ivg_reconciler/__main__.py
@@ -2,8 +2,9 @@ import sys
 
 
 def _self_test():
-    from .core import normalize_text, _quarter_end
     from datetime import date
+    from ivg_reconciler.models import normalize_text
+    from ivg_reconciler.matcher import _quarter_end
     assert normalize_text("San Daniele del Friuli") == "SAN DANIELE DEL FRIULI"
     assert _quarter_end(date(2026, 2, 1)) == date(2026, 3, 31)
 
@@ -12,5 +13,5 @@ if __name__ == "__main__":
     if "--self-test" in sys.argv:
         _self_test()
     else:
-        from .gui import main
+        from ivg_reconciler.gui import main
         main()
